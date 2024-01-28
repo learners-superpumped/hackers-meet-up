1146b674da7bfe253ce69891f80e0703e9929fe6
GwonHyeok <me@ghyeok.io>
2024-01-27 17:06:18 -0800

    [frontend] dashboard design

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,113 +1,32 @@
-import Image from "next/image";
+'use client';
+import {useEffect} from "react";
+import {useRouter} from "next/navigation";
+import classNames from "classnames";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+
+    const router = useRouter()
+
+    useEffect(() => {
+        if (typeof window === 'undefined') return
+        const userId = sessionStorage.getItem('AGIParty/userid')
+        if (userId === null) {
+            router.push('/auth/login')
+        } else {
+            router.push('/')
+        }
+    }, [])
+
+    return (
+        <div className={classNames('w-full', 'h-screen', 'flex', 'justify-center', 'items-center')}>
+            <div
+                className={classNames('animate-spin w-[72px] h-[72px]', 'justify-center', 'items-center', 'text-center')}>
+                <svg width="73" height="72" viewBox="0 0 73 72" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M36.5 12V6C32.5603 6 28.6593 6.77597 25.0195 8.28361C21.3797 9.79126 18.0726 12.001 15.2868 14.7868C9.66071 20.4129 6.5 28.0435 6.5 36H12.5C12.5 29.6348 15.0286 23.5303 19.5294 19.0294C24.0303 14.5286 30.1348 12 36.5 12Z"
+                        fill="#1570FF"/>
+                </svg>
+            </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    );
 }
---
+++ frontend/src/app/survey/components/question.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import {SurveyQuestion} from "@/model/survey";
+import React, {useMemo, useState} from "react";
+import classNames from "classnames";
+
+export interface SurveyQuestionItemProps extends React.HTMLAttributes<HTMLDivElement> {
+    question: SurveyQuestion;
+    onAnswer: (question: SurveyQuestion, answer: string) => void;
+}
+
+export function SurveyQuestionItem({question, ...props}: SurveyQuestionItemProps) {
+
+    const [answer, setAnswer] = useState<string>()
+    const isValid = useMemo(() => answer, [answer])
+
+    return <div{...props}>
+        <div className={classNames('w-[860px]', 'text-start', 'flex', 'flex-col')}>
+
+            <span className={classNames('font-semibold', 'text-black', 'text-[40px]')}>
+                {question.item}
+            </span>
+
+            <input
+                autoFocus
+                onKeyDown={(event) => {
+                    // Enter 입력시 setAnswer
+                    if (event.keyCode === 13 && answer) props.onAnswer(question, answer)
+                }}
+                placeholder="Type Answer"
+                onChange={(e) => setAnswer(e.target.value)}
+                className={classNames('mt-6', 'w-full', 'border-b-2', 'py-6', 'text-[32px]')}>
+
+            </input>
+
+            <div
+                onClick={() => answer && props.onAnswer(question, answer)}
+                className={classNames('w-fit h-[4.375rem] px-6 py-3 rounded-xl justify-center items-center cursor-pointer', 'mt-6',
+                    isValid ? 'bg-black' : 'bg-zinc-200')}>
+                <span className="text-white text-[32px] font-semibold">↳ Enter</span>
+            </div>
+        </div>
+    </div>
+}
