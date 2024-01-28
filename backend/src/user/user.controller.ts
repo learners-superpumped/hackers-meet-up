cdae58391e4421f76dbf11edc5c72bb2564bf63e
Jinu Noh <yes.jinu@gmail.com>
2024-01-27 16:26:25 -0800

    feat: implement get by id, update by id

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) { }
+
+  @Get(':id')
+  async getUserById(@Param('id') id: number) {
+    console.log('id', id);
+    return await this.userService.getUserById(id);
+  }
+
+  @Patch(':id')
+  async updateUserById(
+    @Param('id') id: number,
+    @Body()
+    body: {
+      name?: string;
+      linkedInUrl?: string;
+    },
+  ) {
+    return await this.userService.updateUserById(id, body);
+  }
+}
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { supabase } from '../db/supabase.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  async getUserById(id: number) {
+    const result = await supabase.from('user').select('*').eq('id', id);
+
+    return result.data;
+  }
+
+  async updateUserById(
+    id: number,
+    body: { name?: string; linkedInUrl?: string },
+  ) {
+    await supabase
+      .from('user')
+      .update({
+        name: body.name,
+        linkedInUrl: body.linkedInUrl,
+      })
+      .eq('id', id);
+  }
+}
